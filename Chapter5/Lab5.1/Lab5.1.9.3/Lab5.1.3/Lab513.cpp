c65785de244a44048a3f106ebb6d74e889664b21
matsapura98 <matsapura98@gmail.com>
2017-12-17 13:55:57 +0200

    Chapter5

---
+++ Chapter5/Lab5.1/Lab5.1.9.3/Lab5.1.3/Lab513.cpp
@@ -0,0 +1,68 @@
+// Lab513.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+class AdHocSquare
+{
+	public:
+		AdHocSquare(double side)
+		{
+			this->side = side;
+		}
+		void set_side(double side)
+		{
+			this->side = side;
+		}
+		double get_area()
+		{
+			return side * side;
+		}
+	private:
+		double side;
+};
+class LazySquare
+{
+	public:
+		LazySquare(double side)
+		{
+			this->side = side;
+			side_changed = false;
+			area = side * side;
+		}
+		void set_side(double side)
+		{
+			this->side = side;
+			side_changed = true;
+		}
+		double get_area()
+		{
+			area = side * side;
+			side_changed = false;
+			return area;
+		}
+	private:
+		double side;
+		double area;
+		bool side_changed;
+};
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	AdHocSquare ahs = AdHocSquare(5);
+	LazySquare ls = LazySquare(4);
+
+	cout << "AdHocSquare area = " << ahs.get_area() << endl;
+	cout << "LazySquare area = " << ls.get_area() << endl;
+
+	ahs.set_side(6);
+	ls.set_side(5);
+
+	cout << "AdHocSquare area = " << ahs.get_area() << endl;
+	cout << "LazySquare area = " << ls.get_area() << endl;
+
+    return 0;
+}
+
